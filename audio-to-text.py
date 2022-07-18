cb7f860c759987a635e19bed83da5faafc3f2d1e
rahafibrahim21 <108196720+rahafibrahim21@users.noreply.github.com>
2022-07-18 06:24:23 +0300

    Add files via upload

---
+++ audio-to-text.py
@@ -0,0 +1,16 @@
+import speech_recognition as sr
+
+# Record Audio
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+# Speech recognition using Google Speech Recognition
+try:
+    print("You said: " + r.recognize_google(audio,language ="ar-AR"))
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
